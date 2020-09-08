9f875f480db4e8205863058f93bdcb1c7004cdf2
Ohidur Rahman Bappy <ohidurbappy@gmail.com>
2020-09-09 01:05:07 +0600

    See #1 - Added random_string()

---
+++ reusable/functions.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+# Part of reusable package
+# 
+# Copyright (c) 2020 - Ohidur Rahman Bappy - MIT License
+"""String Related Functions.
+
+Contains a handful of string related functions.
+"""
+
+from .string_functions import random_string
---
+++ reusable/string_functions.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+# Part of reusable package
+# 
+# Copyright (c) 2020 - Ohidur Rahman Bappy - MIT License
+"""String Related Functions.
+
+Contains a handful of string related functions.
+"""
+
+import random
+import string
+
+__all__=[
+    'random_string'
+]
+
+def random_string(length:int=6, charset:str=string.ascii_letters+string.digits):
+    r"""Return a random string of given length and charset.
+
+    Default charset is url-friendly (base62).
+
+    Parameters::
+
+        length (int): The length of the string to return [Optional]
+        charset (str): The character set to build the string
+
+    Usage::
+
+        >>> from reusable.functions import *
+        >>> print(random_string(5))
+
+
+    """
+    return ''.join([random.choice(charset) for i in range(length)])
