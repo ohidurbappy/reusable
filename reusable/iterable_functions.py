e8bb1c06cd5d7b04bcff90ffd6ca76254cbf9a4e
Ohidur Rahman Bappy <ohidurbappy@gmail.com>
2020-09-24 01:19:15 +0600

    Added groupby_count function

---
+++ reusable/iterable_functions.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+# Part of reusable package
+# 
+# Copyright (c) 2020 - Ohidur Rahman Bappy - MIT License
+"""Iterable Functions.
+
+Contains a handful of iterable functions.
+"""
+
+
+
+
+from collections import defaultdict
+
+
+def groupby_count(i, key=None, force_keys=None):
+    """Group a list of elements as per the count
+    
+    Usage::
+
+        >>> from reusable.functions import groupby_count
+        >>> print(groupby_count([1,1,1,2,3]))
+        [(1,3),(2,1),(3,1)]
+    """
+    counter = defaultdict(lambda: 0)
+    if not key:
+        key = lambda o: o
+
+    for k in i:
+        counter[key(k)] += 1
+
+    if force_keys:
+        for k in force_keys:
+            counter[k] += 0
+
+    return counter.items()
